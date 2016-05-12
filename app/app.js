8f68ab569012b9edd873269d0b98eeff6d5a6562
caraclarke <cara.clarke8@gmail.com>
2016-05-12 10:27:29 -0400

    Updated app and index files. Added template directory and empty files

--- app/app.js
+++ app/app.js
@@ -1,12 +1,13 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('templateStore', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  'templateStore.view1',
+  'templateStore.view2',
+  'templateStore.templates'
+])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/templates'});
 }]);
